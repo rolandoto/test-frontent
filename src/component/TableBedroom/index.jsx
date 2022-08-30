8a9feebf23b419aba0ae6028e59fcdd8ea1afc7d
ghInovacion <programacion@grupo-hoteles.com>
2022-08-30 13:46:12 -0500

    update rooms view

--- src/component/TableBedroom/index.jsx
+++ src/component/TableBedroom/index.jsx
@@ -1,22 +1,7 @@
 import React, { useState } from "react"
 import { useEffect } from "react"
 
-
-
-
-
-
 const TableBedRoom =({id}) =>{
-    const [results,setResults] = useState();
-
-
-    const Prueba = ({re}) =>{
-        
-            const url = `https://grupohoteles.co/api/getTypeRoomByID?id_tipo_habitacion=2`
-            return fetch(url)
-            .then(res => res.json())
-            .then(data => data)
-     }
      
     const [state,setState] = useState()
 
@@ -24,20 +9,8 @@ const TableBedRoom =({id}) =>{
         fetch(`http://localhost:4000/api/admin/getroomsadmin/${id}`)
         .then(resp => resp.json())
         .then(data => setState(data))
-        resultls()
-    },[])
+    }, [])
 
-    const pokemonsArray = [];
-         
-    const resultls = async () => {
-    
-          for await (const index of state.customer) {
-            pokemonsArray.push(index.ID_Tipo_habitaciones)
-          }
-      };
-    
-    
-      //console.log(results)
     return (
         <div className="container-table" >
             <table className="table" >
@@ -51,13 +24,17 @@ const TableBedRoom =({id}) =>{
                     <th>Cantidad maximos de personas</th>
                 </tr>
 
-                    {state?.customer.map(index =>{
-                       
-                       let dataTypeRoom = Prueba(index.ID_Tipo_habitaciones);
-                        console.log(dataTypeRoom)
+                    {state?.ray.map(index =>{
+
                         return (
                             <tr>
+                                <td>{index.nombre}</td>
                                 <td>{index.nombreEstado}</td>
+                                <td>{index.Numero}</td>
+                                <td>{index.precio}</td>
+                                <td>{index.precio_persona}</td>
+                                <td>{index.persona}</td>
+                                <td>{index.max_persona}</td>
                             </tr>
                         )
                     })}
