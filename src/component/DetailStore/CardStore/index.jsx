c0d976b1052c939f2b3a2478e9b4eac5497d3dcb
ghInovacion <programacion@grupo-hoteles.com>
2022-09-01 11:21:55 -0500

    style: modific style DetailStore

--- src/component/DetailStore/CardStore/index.jsx
+++ src/component/DetailStore/CardStore/index.jsx
@@ -5,28 +5,28 @@ const CardStore =() =>{
     return (
         <div>
             <ul  className="container-card" >
-                <li className="card-Bedrrom" >
+                <li className="card-stores" >
                     <div>
                         <h1 className="title-card" >30.0000</h1>
                         <span>Habitacion</span>
                     </div>
                 </li>
 
-                <li className="card-Bedrrom" >
+                <li className="card-stores" >
                     <h1 className="title-card" >4</h1>
                     <span>Tipos de habitaciones</span>
                 </li>
 
-                <li className="card-Bedrrom" >
+                <li className="card-stores" >
                     <h1 className="title-card" >Med</h1>
                     <span>Ciudad</span>
                 </li>
 
-                <li className="card-Bedrrom" >
+                <li className="card-stores" >
                     <h1 className="title-card" >26.000.00</h1>
                     <span>Proyeccion MES 80%</span>
                 </li>
-                <li className="card-Bedrrom" >
+                <li className="card-stores" >
                     <h1 className="title-card"  >86.000.000</h1>
                     <span>Proyecion ANO 80%</span>
                 </li>
