c74656e7003b39645b561c5cd9687b08f582c007
Jhonathan Velez <kokoa.arte@gmail.com>
2024-03-20 15:52:28 -0500

    update commit uselocalstoreage

---
+++ src/hooks/UselocalStorage.jsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from "react"
+
+
+const Uselocalstorage = (key,initalvalue) => {
+   
+  const [storage,setStorage] = useState(() => {
+  
+    try{
+          const item = window.localStorage.getItem(key) 
+          return item != null ? JSON.parse(item):initalvalue
+  }catch (e) {
+        console.log(e)
+     }
+
+  })
+
+  const setValue =(evento)=> {
+     
+    try {
+          window.localStorage.setItem(key,JSON.stringify(evento))
+          setStorage(evento)
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+    const Remove = () => {
+        const item =  window.localStorage.removeItem(key)
+        setStorage(null)
+        return item ? null: initalvalue
+
+    }
+   
+  return [storage,setValue,Remove]
+}
+
+
+export default Uselocalstorage
