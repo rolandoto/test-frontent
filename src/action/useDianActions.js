27e19f48891ee44a01e47ab2745c2db39c65104c
Rolando Guerrero <rolando22_@outlook.com>
2024-03-30 18:42:52 -0500

    Update useDianActions.js
    update commit

--- src/action/useDianActions.js
+++ src/action/useDianActions.js
@@ -100,6 +100,15 @@ const UseDianActions =() =>{
                         })).catch(e =>{
                             toast.error("error al insertar en el reserva")
                         })
+                }else{
+                         HttpClient.PostInsertSigOpdfbyid({  id:id_Reserva,id_sigo:  "123456789" }).then((item => {
+                        history.push(`/DetailDashboard/${id_Reserva}`);
+                         dispatch(setDian(itemResponse));
+                        dispatch(setPayment(itemResponse));
+                            toast.success("Se guarado correctamente la facturacion")
+                        })).catch(e =>{
+                            toast.error("error al insertar en el reserva")
+                        })
                 }
                  
             })).catch(e =>{
