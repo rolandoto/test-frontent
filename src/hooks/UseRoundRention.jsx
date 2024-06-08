430cd69723dac3625fc93402350b10e68a650e03
Rolando Guerrero <rolando22_@outlook.com>
2024-06-08 15:55:40 -0700

    Update UseRoundRention.jsx

--- src/hooks/UseRoundRention.jsx
+++ src/hooks/UseRoundRention.jsx
@@ -1,11 +1,11 @@
 
 const UseRoundRention =({Price=0}) =>{
 
-        function roundTo(value, decimals) {
+          function roundTo(value, decimals) {
             return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
         }
         const RountValue =(round) =>{
-          return  Math.round(round * 10000000) / 10000000;
+          return  Math.round(round * 100000) / 100000;
         }
         
         const CalculateRetention =(ValorProduct) =>{
@@ -14,18 +14,18 @@ const UseRoundRention =({Price=0}) =>{
             let tasaRetefuente = 0.035;
             let factor = 1 + tasaIVA - tasaRetefuente;
             let subtotal = totalDeseado / factor;
-            subtotal = roundTo(subtotal, 6);
+            subtotal = roundTo(subtotal, 5);
             let iva = subtotal * tasaIVA;
-            iva = roundTo(iva, 6);
+            iva = roundTo(iva, 2);
             let retefuente = subtotal * tasaRetefuente;
-            retefuente = roundTo(retefuente, 6);
-            let total = roundTo(subtotal + iva - retefuente, 6);
+            retefuente = roundTo(retefuente, 2);
+            let total = roundTo(subtotal + iva - retefuente, 2);
             if (total !== totalDeseado) {
                 let adjustment = totalDeseado - total;
                 subtotal += adjustment;
-                iva = roundTo(subtotal * tasaIVA, 6);
-                retefuente = roundTo(subtotal * tasaRetefuente, 6);
-                total = roundTo(subtotal + iva - retefuente, 6);
+                iva = roundTo(subtotal * tasaIVA, 2);
+                retefuente = roundTo(subtotal * tasaRetefuente, 2);
+                total = roundTo(subtotal + iva - retefuente, 2);
             }
           return {subtotal,retefuente,total,iva}
         }
