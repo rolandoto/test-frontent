5920d5e11d6da94404c43d7c9a9ef4e324b15f63
Rolando Guerrero <rolando22_@outlook.com>
2024-06-20 20:30:25 -0700

    Update index.jsx

--- src/templates/Store/index.jsx
+++ src/templates/Store/index.jsx
@@ -916,16 +916,18 @@ const StoreTemplate =({Store}) =>{
       content: () => componentRef.current
     });
 
-    const handSubmitInsertCartOne =() =>{
-        setPreloading(true) 
-      ServiceaInsertStore({data:dataOne}).then(index =>{
-        handlePrint()
-        setPreloading(false)
-      }).catch(e=> {
-           setPreloading(false)
-         console.log(e)
-      })
+   const handSubmitInsertCartOne = async () => {
+    try {
+        setPreloading(true); // Indica que la carga ha comenzado
+        await ServiceaInsertStore({ data: dataOne }); // Espera a que la llamada a la API se complete
+        handlePrint(); // Llama a handlePrint después de que la API se complete con éxito
+    } catch (e) {
+        console.log(e); // Maneja cualquier error que ocurra durante la llamada a la API
+    } finally {
+        setPreloading(false); // Asegura que setPreloading(false) se ejecute independientemente del éxito o fracaso de la llamada a la API
     }
+};
+    
 
     const {cart} = carts
     const currenCart =[]
