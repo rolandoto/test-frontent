025caf6b5edec70dbc22c725ab23d188f7ef6a8f
Jhonathan Velez <kokoa.arte@gmail.com>
2024-06-26 16:43:28 -0500

    intial commidasd

---
+++ src/component/DetailStore/DetailStoreUpdate/index.jsx
@@ -0,0 +1,143 @@
+import React, { useContext, useEffect, useState } from "react"
+import  AutoProvider  from "../../../privateRoute/AutoProvider"
+import { useHistory } from "react-router-dom"
+import { CiSquarePlus } from "react-icons/ci";
+import { GiPencil } from "react-icons/gi";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { config } from "../../../config";
+import PostUpdateStoreProduct from "../../../service/PostUpdateStoreProduct";
+import Swal from "sweetalert2";
+
+
+const DetailStoreUpdate =() =>{
+
+    const {id}  = useParams()
+    const [state,setState] =useState()
+    const [produ,setPordu] =useState()
+    const {jwt} = useContext(AutoProvider)
+
+    
+    const handEffect =() => {
+        fetch(`${config.serverRoute}/api/admin/getproduct/${id}`)
+        .then(resp => resp.json())
+        .then(data => setState(data.query))
+
+        fetch(`${config.serverRoute}/api/admin/getProdcutUpdte/${id}`)
+        .then(resp => resp.json())
+        .then(data => setPordu(data.query))
+    }
+    
+    const Product = state?.find(index => index.ID == id);
+
+    const [cantidad, setCantidad] = useState();
+    const [Price, setPrice] = useState();
+
+    const PriceValue =  Price ? Price :   Product?.Precio
+    const CantidadValues =  cantidad? cantidad   : Product?.Cantidad
+    
+
+    useEffect(() =>{
+        handEffect()
+    },[])
+
+
+   const handCLickServiceInsertStore =() =>{
+        PostUpdateStoreProduct({ID:id,Cantidad:CantidadValues,ID_user:jwt?.result?.id_user,Price:PriceValue}).then(index => {
+            handEffect()
+            setCantidad(0)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '<p>Exitoso</p>',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        }).catch(e =>{
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: '<p>error al agregar</p>',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        })
+   }
+
+   
+
+   if(!state) return null 
+  
+
+    return (<div>
+          <div className="container-bicta">
+            <tbody>
+                <table className="de">
+                    <tr>
+                        <th>Categoria</th>
+                        <th>Nombre</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Nombre Recepcion</th>
+                    </tr>
+                    {produ?.map(index =>{
+                        return (
+                            <tr>
+                                <td>{index.categoria}</td>
+                                <td>{index.Nombre}</td>
+                                <td>{index.Cantidad_total}</td>
+                                <td>{index.Price}</td>
+                                <td>{index.name}</td>
+                    </tr>)})}
+                </table>
+            </tbody>
+
+            <tbody>
+                <table className="de">
+                    <tr>
+                        <th>Cantidad</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Opciones</th>
+                    </tr>
+                    <tr>
+                            <td>  
+                            <input 
+                                className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                name="Correo" 
+                                type="text"
+                                defaultValue={Product?.Nombre}
+                            />
+                        </td>
+                        <td>  
+                        <input 
+                                className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                type="number" 
+                                name="cantidad" 
+                                placeholder="No Documento"  
+                                onChange={(e) => setCantidad(e.target.value)}
+                                defaultValue={Product?.Cantidad}
+                            />
+                        </td>
+                        <td>  
+                        <input 
+                                className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                name="Correo" 
+                                type="number"
+                                onChange={(e) => setPrice(e.target.value)}
+                                defaultValue={Product?.Precio}
+                            />
+                        </td>
+                        
+                        
+                        <td><button  className="button-Border-Id"   onClick={handCLickServiceInsertStore} > <CiSquarePlus     fontSize={35} /> </button></td>
+                    </tr>
+                    
+                           
+                </table>
+            </tbody>
+        </div>
+            </div>)
+
+}
+
+export default DetailStoreUpdate
---
+++ src/service/PostUpdateStoreProduct.jsx
@@ -0,0 +1,19 @@
+import { config } from "../config"
+
+const ENDPOINT =`${config.serverRoute}/api/admin/updateProduct/`
+
+const PostUpdateStoreProduct =({ID,Cantidad,ID_user,Price})=>{
+    return fetch(`${ENDPOINT}`,{
+        method:'POST',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({ID,Cantidad,ID_user,Price})
+    }).then(resp =>{
+        if(!resp.ok) throw new Error('Response is not ok')
+        return resp.json()
+    }).then(resp=>{
+        return resp
+    })
+}
+export default PostUpdateStoreProduct
