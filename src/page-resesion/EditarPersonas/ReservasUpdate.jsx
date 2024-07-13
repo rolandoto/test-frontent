6f208bcb5464a2b7574128054ac15235d583ff9e
Jhonathan Velez <kokoa.arte@gmail.com>
2024-07-12 19:49:32 -0500

    intial commit udate

--- src/page-resesion/EditarPersonas/ReservasUpdate.jsx
+++ src/page-resesion/EditarPersonas/ReservasUpdate.jsx
@@ -21,27 +21,32 @@ import { Button } from "@nextui-org/react";
 
 
 function calcularDV(cedula) {
+    // Verificar si la cédula está vacía o no es un número válido
+    if (!cedula || isNaN(parseInt(cedula))) {
+        return 0; // Retornar 0 si la cédula no está definida o no es válida
+    }
+
     // Secuencia de multiplicación
     const secuencia = [3, 7, 13, 17, 19, 23, 29, 37, 41, 43];
     let suma = 0;
-    
+
     // Convertir la cédula a string y revertirla
     const cedulaStr = cedula.toString().split('').reverse().join('');
-    
+
     // Multiplicar cada dígito por el número correspondiente en la secuencia
     for (let i = 0; i < cedulaStr.length; i++) {
-      suma += parseInt(cedulaStr[i]) * secuencia[i % secuencia.length];
+        suma += parseInt(cedulaStr[i]) * secuencia[i % secuencia.length];
     }
-    
+
     // Calcular el residuo de la suma dividida por 11
     const residuo = suma % 11;
-    
+
     // Restar el residuo de 11 para obtener el dígito de verificación
     const dv = 11 - residuo;
-    
+
     // Si el resultado es 10 o mayor, el dígito de verificación es 0
     return dv >= 10 ? 0 : dv;
-  }
+}
 
 const LoadingSkeleton =(props) =>{
     return (      <ContentLoader height={1000} width={700} >
@@ -215,14 +220,17 @@ const ReservasUpdate =(props) =>{
     const cedula = resultDasboard.Num_documento;
     const dv = calcularDV(cedula);
 
+    console.log(dv)
+
     const validDefinidName = person =="Company" ?  
-    [
+    [ [
         `${resultDasboard.Nombre,resultDasboard.Apellido}`
-    ].join(""): 
+    ].join("")] : 
     [
         resultDasboard.Nombre,
         resultDasboard.Apellido
     ] 
+
    
     const body = {
         "type": "Customer",
@@ -230,9 +238,7 @@ const ReservasUpdate =(props) =>{
         "id_type": `${docu?.ID_document_dian}`,
         "identification": resultDasboard.Num_documento,
         "check_digit":`${dv}`,
-        "name": [
-            validDefinidName
-        ],
+        "name":validDefinidName ,
         "commercial_name":  resultDasboard.Nombre,
         "branch_office": 0,
         "active": true,
