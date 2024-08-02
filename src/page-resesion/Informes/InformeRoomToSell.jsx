f7ad8cf40d328a0517591069c5426da236769e64
Jhonathan Velez <kokoa.arte@gmail.com>
2024-08-02 16:09:05 -0500

    initial commit update}

--- src/page-resesion/Informes/InformeRoomToSell.jsx
+++ src/page-resesion/Informes/InformeRoomToSell.jsx
@@ -124,7 +124,7 @@ const InformeRoomToSell =() =>{
                             </div>
                             <div className=" ">
                         <div className="w-full overflow-x-auto">
-                            <table className="min-w-full bg-white shadow-md rounded border border-gray-200" ref={componentRef}>
+                            <table className="table min-w-full bg-white shadow-md rounded border border-gray-200" ref={componentRef}>
                             <thead>
                                 <tr>
                                 <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
